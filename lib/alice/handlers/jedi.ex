55285c38de452fe2640767106e5e6b323a7212c1
Nathaniel Barnes <nathaniel.r.barnes@gmail.com>
2020-06-10 20:06:02 -0700

    Add route for this is the way

--- lib/alice/handlers/jedi.ex
+++ lib/alice/handlers/jedi.ex
@@ -1,3 +1,9 @@
 defmodule Alice.Handlers.Jedi do
+  use Alice.Router
 
+  route ~r/^this is the way/i, :the_way_response
+
+  def the_way_response(conn) do
+    reply("This is the way", conn)
+  end
 end
--- test/alice/handlers/jedi_test.exs
+++ test/alice/handlers/jedi_test.exs
@@ -1,3 +1,15 @@
 defmodule Alice.Handlers.JediTest do
+  use Alice.HandlerCase, handlers: Alice.Handlers.Jedi
 
+  test "it responds to 'This is the way'" do
+    send_message("This is the way")
+
+    assert first_reply() == "This is the way"
+  end
+
+  test "it doesn't respond if it's used in a sentance" do
+    send_message("Blah blah blah, this is the way")
+
+    assert first_reply() == nil
+  end
 end
