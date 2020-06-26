2830ce9f41bd31425975ab27243b4c12bbce9a65
Nathaniel Barnes <nathaniel.r.barnes@gmail.com>
2020-06-25 21:31:31 -0700

    Update license and links

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule JediAlice.MixProject do
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      description: "An addon for Alice to display Star Wars themed responses"
     ]
   end
 
@@ -22,6 +24,16 @@ defmodule JediAlice.MixProject do
   defp deps do
     [
       {:alice, "~> 0.4.3"}
+    ]
+  end
+
+  defp package() do
+    [
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/NateBarnes/jedi_alice",
+        "Docs" => "https://hexdocs.pm/jedi_alice/0.1.0"
+      }
     ]
   end
 end
